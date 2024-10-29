91fb7cfd87689b1921c9443e0e4fca65707a5b8c
BSIDOMAIN\BSI90887 <bsi90887@bsi.co.id>
2024-10-29 23:16:37 +0700

    Improve AuthPrivilegeAttribute

--- EasyResto/Middleware/AuthPrivilegeAttribute.cs
+++ EasyResto/Middleware/AuthPrivilegeAttribute.cs
@@ -1,7 +1,9 @@
-﻿using EasyResto.Domain.Enums;
+﻿using EasyResto.Domain.Common;
+using EasyResto.Domain.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace EasyResto.Middleware
 {
@@ -18,14 +20,26 @@ namespace EasyResto.Middleware
         {
             if (!context.HttpContext.User.Identity.IsAuthenticated)
             {
-                context.Result = new UnauthorizedResult();
+                var response = new BaseResponse<string>();
+                response.Status = (int)HttpStatusCode.Unauthorized;
+                response.Title = "Unauthorized";
+                response.Message = "You are not authorized";
+                response.Errors = new List<string> { "You are not authorized" };
+
+                context.Result = new ObjectResult(response) { StatusCode = (int)HttpStatusCode.Unauthorized };
                 return;
             }
 
             List<string> privileges = Privilege.Split(',').Select(privilege => privilege.Trim()).ToList();
             if (!context.HttpContext.User.HasClaim(c => c.Type == AuthCode.Privilege.ToString() && privileges.Contains(c.Value)))
             {
-                context.Result = new ForbidResult();
+                var response = new BaseResponse<string>();
+                response.Status = (int)HttpStatusCode.Forbidden;
+                response.Title = "Forbidden";
+                response.Message = "You don't have permission";
+                response.Errors = new List<string> { "You don't have permission" };
+
+                context.Result = new ObjectResult(response) { StatusCode = (int)HttpStatusCode.Forbidden };
                 return;
             }
         }
